971e4c063e7d0ee4a36f763b7b6e6b1c28ddeed8
anand kumar <2anandkr@gmail.com>
2013-07-25 12:27:47 +0000

    web.js now fetches html output from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    file_content = fs.readFileSync('index.html');
+    response.send(file_content.toString());
 });
 
 var port = process.env.PORT || 5000;
